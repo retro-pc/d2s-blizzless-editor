5deb4a8ddbd2b6840dc4371ff5d30936b163363a
stanislav <spg_stany@gmail.com>
2025-08-25 21:18:27 +0300

    release-add-blizzless-support

--- src/components/inventory/Stash.vue
+++ src/components/inventory/Stash.vue
@@ -1,78 +1,109 @@
 <template>
   <div class="stash">
-    <div
-          class="btn-group"
-          role="group">
-        <button
-            type="button"
-            class="btn btn-secondary"
-            :class="{ active: activeTab == 1 }"
-            @click="changeTab(1)">Personal
-        </button>
-        <button
-            type="button"
-            class="btn btn-secondary"
-            :class="{ active: activeTab == 2 }"
-            @click="changeTab(2)">Shared
-        </button>
-        <button
-            type="button"
-            class="btn btn-secondary"
-            :class="{ active: activeTab == 3 }"
-            @click="changeTab(3)">Shared
-        </button>
-        <button
-            type="button"
-            class="btn btn-secondary"
-            :class="{ active: activeTab == 4 }"
-            @click="changeTab(4)">Shared
-        </button>
+    <div class="btn-group" role="group">
+      <button
+          type="button"
+          class="btn btn-secondary"
+          :class="{ active: activeTab == 1 }"
+          @click="changeTab(1)"
+      >
+        Personal
+      </button>
+      <button
+          type="button"
+          class="btn btn-secondary"
+          :class="{ active: activeTab == 2 }"
+          @click="changeTab(2)"
+      >
+        Shared
+      </button>
+      <button
+          type="button"
+          class="btn btn-secondary"
+          :class="{ active: activeTab == 3 }"
+          @click="changeTab(3)"
+      >
+        Shared
+      </button>
+      <button
+          type="button"
+          class="btn btn-secondary"
+          :class="{ active: activeTab == 4 }"
+          @click="changeTab(4)"
+      >
+        Shared
+      </button>
     </div>
-    <div class="stash-bg">
+    <!-- переключатель D2R/D2R-BLIZZ -->
+    <!-- <div class="mode-toggle">
+      <div class="btn-group">
+        <button
+            type="button"
+            class="btn btn-outline-secondary"
+            :class="{ active: gameMode === 'd2r' }"
+            @click="gameMode = 'd2r'"
+        >
+          LoD
+        </button>
+        <button
+            type="button"
+            class="btn btn-outline-secondary"
+            :class="{ active: gameMode === 'd2r-blizz' }"
+            @click="gameMode = 'd2r-blizz'"
+        >
+          D2R
+        </button>
+      </div>
+    </div>-->
+    <div class="stash-bg":class="{'stash-bg-big': gameMode === 'd2r-blizz'}">
       <Grid
           v-if="activeTab == 1"
-          :width="grid.stash.w"
-          :height="grid.stash.h"
+          :width="stashGrid.width"
+          :height="stashGrid.height"
           :page="1"
           :items.sync="stash(0)"
           @item-selected="onSelect"
           @item-event="onEvent"
           :id="'Grid'"
-          :contextMenu=contextMenu
-          class="y-0"></Grid>
+          :contextMenu="contextMenu"
+          class="y-0"
+      ></Grid>
       <Grid
           v-if="activeTab == 2"
-          :width="grid.stash.w"
-          :height="grid.stash.h"
+          :width="stashGrid.width"
+          :height="stashGrid.height"
           :page="2"
           :items.sync="stash(1)"
           @item-selected="onSelect"
           @item-event="onEvent"
           :id="'Grid'"
-          :contextMenu=contextMenu
-          class="y-0"></Grid>
+          :contextMenu="contextMenu"
+          class="y-0"
+      ></Grid>
       <Grid
           v-if="activeTab == 3"
-          :width="grid.stash.w"
-          :height="grid.stash.h"
+          :width="stashGrid.width"
+          :height="stashGrid.height"
           :page="3"
           :items.sync="stash(2)"
           @item-selected="onSelect"
           @item-event="onEvent"
           :id="'Grid'"
-          :contextMenu=contextMenu
-          class="y-0"></Grid>
+          :contextMenu="contextMenu"
+          class="y-0"
+      ></Grid>
       <Grid
           v-if="activeTab == 4"
-          :width="grid.stash.w"
-          :height="grid.stash.h"
+          :width="stashGrid.width"
+          :height="stashGrid.height"
           :page="4"
           :items.sync="stash(3)"
           @item-selected="onSelect"
           @item-event="onEvent"
           :id="'Grid'"
-          :contextMenu=contextMenu
-          class="y-0"></Grid>
+          :contextMenu="contextMenu"
+          class="y-0"
+      ></Grid>
     </div>
   </div>
 </template>
@@ -92,7 +123,7 @@ export default {
   data() {
     return {
       activeTab: 1,
-      grid: {stash: {w: 10, h: 10}},
+      gameMode: 'd2r-blizz'
     };
   },
   props: {
@@ -100,7 +131,13 @@ export default {
     id: String,
     contextMenu: Object,
   },
-  computed: {},
+  computed: {
+    stashGrid() {
+      return this.gameMode === 'd2r-blizz'
+          ? { width: 16, height: 13 }
+          : { width: 10, height: 10 };
+    }
+  },
   methods: {
     onSelect(item) {
       this.$emit('item-selected', item);
@@ -115,3 +152,17 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.mode-toggle {
+  margin: 10px 0;
+  display: flex;
+}
+
+/* Стили для активной кнопки переключателя */
+.btn-outline-secondary.active {
+  background-color: #6c757d;
+  color: white;
+  border-color: #6c757d;
+}
+</style>
